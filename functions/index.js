582f9f0c0cce89bca51c327ac4f30f85fa992a2a
JulioAvalos <julioavalos2@msn.com>
2020-08-31 21:47:09 -0600

    Using nodemailer dependency to send mails

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,35 @@
 const functions = require('firebase-functions');
-const config = functions.config();
+
 const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+const config = functions.config();
+const cors = require('cors')({origin: true});
+
+admin.initializeApp();
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: { user: config.user.email, pass: config.user.password }
+});
+
+let mailOptions = {
+    from: "Arc Development", 
+    to: "your_email_lol", 
+    subject: "Testing nodemailer",
+    text: "Test successful"
+};
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+exports.sendMail = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        transporter.sendMail(mailOptions, error => {
+            if(error) {
+                response.send(error);
+            } else {
+                response.send("Message sent successfully");
+            }
+        });
+    });
+});
