ae95f301e26192c138d4d89da35625a1b36b4e44
JulioAvalos <julioavalos2@msn.com>
2020-08-31 22:25:56 -0600

    Using Snackbar

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,6 +11,7 @@ import ButtonArrow from './UI/ButtonArrow';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import CirculaProgress from '@material-ui/core/CircularProgress';
+import Snackbar from '@material-ui/core/Snackbar';
 
 import background from '../assets/background.jpg';
 import mobileBackground from '../assets/mobileBackground.jpg';
@@ -99,6 +100,8 @@ const Contact = props => {
 
     const [loading, setLoading] = useState(false);
 
+    const [alert, setAlert] = useState({open: false, message: "", backgroundColor: ""});
+
     const onChange = event => {
         let valid;
 
@@ -139,10 +142,19 @@ const Contact = props => {
             setEmail("");
             setPhone("");
             setMessage("");
-            console.log(res);
+            setAlert({
+                open: true,
+                message: "Message sent successfully!",
+                backgroundColor: "#4BB543"
+            });
         })
         .catch(err => {
-            setLoading(false)
+            setLoading(false);
+            setAlert({
+                open: true,
+                message: "Something went wrong, please try again!",
+                backgroundColor: "#FF3232"
+            });
         });
     }
 
@@ -384,6 +396,18 @@ const Contact = props => {
                     </Grid>
                 </DialogContent>
             </Dialog>
+            <Snackbar 
+                open={alert.open} 
+                message={alert.message}
+                ContentProps={{
+                    style: {
+                        backgroundColor: alert.backgroundColor
+                    }
+                }}
+                anchorOrigin={{vertical: 'top', horizontal: 'center'}}
+                onClose={()=>setAlert({...alert, open: false})}
+                autoHideDuration={4000}
+            />
             <Grid
                 item
                 container
